37962e36ee5f0411263530616322f473251395e5
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-01-12 02:09:18 +0900

    [ADD] 채팅방의 User table & 연관 관계 설정

---
+++ src/main/java/com/example/codeArena/chatting/domain/ChatRoomUser.java
@@ -0,0 +1,28 @@
+package com.example.codeArena.chatting.domain;
+
+import com.example.codeArena.User.model.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "chat_room_user")
+public class ChatRoomUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+}
