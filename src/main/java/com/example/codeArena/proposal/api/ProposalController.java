3de3c0a5adaf591310a9559012a1a3268236005c
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-01-18 00:51:41 +0900

    [ADD] 신청서 controller TODO API LIST

---
+++ src/main/java/com/example/codeArena/proposal/api/ProposalController.java
@@ -0,0 +1,40 @@
+package com.example.codeArena.proposal.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/test")
+public class ProposalController {
+
+    /**
+     * TODO API LIST
+     */
+
+    /**
+     * 신청서 작성
+     */
+
+    /**
+     * 신청서 하나 조회
+     */
+
+    /**
+     * 사용자가 리더인 모든 방을 조회
+     */
+
+    /**
+     * 사용자가 리더가 아닌(참여자 인) 모든 방을 조회
+     */
+
+    /**
+     * 신청서 수락
+     */
+
+    /**
+     * 신청서 거절
+     */
+}
