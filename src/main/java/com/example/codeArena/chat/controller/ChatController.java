08a931ecc998a7e4d589d70f2d2a1d6b3575521f
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-01-19 02:48:00 +0900

    [ADD] chat 내역 보기 API

--- src/main/java/com/example/codeArena/chat/controller/ChatController.java
+++ src/main/java/com/example/codeArena/chat/controller/ChatController.java
@@ -3,14 +3,20 @@ package com.example.codeArena.chat.controller;
 import com.example.codeArena.chat.dto.request.ChatRequest;
 import com.example.codeArena.chat.dto.response.ChatResponse;
 import com.example.codeArena.chat.service.ChatService;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -31,6 +37,19 @@ public class ChatController {
             @Payload ChatRequest chatRequest
     ) {
         return chatService.save(chatRequest, roomId, simpSessionAttributes);
+    }
+
+    /**
+     * 채팅 목록
+     */
+    @GetMapping("/api/test/room/{roomId}/chats")
+    public ResponseEntity<List<ChatResponse>> getChattingList(
+            @PathVariable(name = "roomId") Long roomId,
+            @PageableDefault(sort = "createdAt") Pageable pageable) {
+
+        List<ChatResponse> response = chatService.getByRoomId(roomId, pageable);
+
+        return ResponseEntity.ok(response);
     }
 
 }
--- src/main/java/com/example/codeArena/chat/service/ChatService.java
+++ src/main/java/com/example/codeArena/chat/service/ChatService.java
@@ -6,11 +6,17 @@ import com.example.codeArena.chat.domain.Chat;
 import com.example.codeArena.chat.dto.request.ChatRequest;
 import com.example.codeArena.chat.dto.response.ChatResponse;
 import com.example.codeArena.chat.repository.ChatRepository;
+import com.example.codeArena.chatroom.domain.ChatRoom;
 import com.example.codeArena.exception.CustomException;
+import com.example.codeArena.exception.CustomException.ErrorCode;
+import com.example.codeArena.proposal.dto.response.ProposalResponse;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,24 +29,38 @@ public class ChatService {
 
     @Transactional
     public ChatResponse save(ChatRequest chatRequest, Long roomId, Map<String, Object> header) {
+        String userNickname = getValueFromHeader(header, "userNickname");
         Chat chat = Chat.builder()
                 .roomId(roomId)
                 .userId(chatRequest.getUserId())
+                .userNickname(userNickname)
                 .messageType(chatRequest.getType())
                 .content(chatRequest.getContent())
                 .build();
         Chat savedChat = chatRepository.save(chat);
-        return toChatResponse(savedChat, header);
-    }
-
-    private ChatResponse toChatResponse(Chat chat, Map<String, Object> header) {
-        String userNickname = getValueFromHeader(header, "userNickname");
-        return Optional.of(chat)
-                .map(c -> new ChatResponse(c, userNickname))
-                .orElseThrow(()->new CustomException(INVALID_INPUT_VALUE_DTO));
+        return toChatResponse(savedChat, userNickname);
     }
 
     private String getValueFromHeader(Map<String, Object> header, String key) {
         return (String)header.get(key);
+    }
+
+    private ChatResponse toChatResponse(Chat chat, String nickname) {
+        return Optional.of(chat)
+                .map(c -> new ChatResponse(c, nickname))
+                .orElseThrow(()->new CustomException(INVALID_INPUT_VALUE_DTO));
+    }
+
+
+
+    public List<ChatResponse> getByRoomId(Long roomId, Pageable pageable) {
+        List<ChatResponse> responses = chatRepository.findByRoomId(roomId, pageable)
+                .map(chat -> {
+                    return Optional.of(chat)
+                            .map(c -> new ChatResponse(c, c.getUserNickname()))
+                            .orElseThrow(()->new CustomException(INVALID_INPUT_VALUE_DTO));
+                })
+                .toList();
+        return responses;
     }
 }
